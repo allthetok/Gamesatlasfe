6f9fc9bd99199ab6c833767202bbcbb2c2806228
allthetok <al.tokjuman@gmail.com>
2023-08-22 21:35:51 -0400

    added assets of ESRB and PEGI ratings

---
+++ src/assets/ratingsvglinks.ts
@@ -0,0 +1,48 @@
+// const ESRB = {
+// 	6: 'https://www.esrb.org/wp-content/uploads/2021/12/RP-LM17-English.svg',
+// 	7: 'https://www.esrb.org/wp-content/uploads/2019/05/RP.svg',
+// 	8: 'https://www.esrb.org/wp-content/uploads/2019/05/E.svg',
+// 	9: 'https://www.esrb.org/wp-content/uploads/2019/05/E10plus.svg',
+// 	10: 'https://www.esrb.org/wp-content/uploads/2019/05/T.svg',
+// 	11: 'https://www.esrb.org/wp-content/uploads/2019/05/M.svg',
+// 	12: 'https://www.esrb.org/wp-content/uploads/2019/05/AO.svg'
+// }
+
+// enum ESRB {
+// 	Six = 'https://www.esrb.org/wp-content/uploads/2021/12/RP-LM17-English.svg',
+// 	Seven = 'https://www.esrb.org/wp-content/uploads/2019/05/RP.svg',
+// 	Eight = 'https://www.esrb.org/wp-content/uploads/2019/05/E.svg',
+// 	Nine = 'https://www.esrb.org/wp-content/uploads/2019/05/E10plus.svg',
+// 	Ten = 'https://www.esrb.org/wp-content/uploads/2019/05/T.svg',
+// 	Eleven =  'https://www.esrb.org/wp-content/uploads/2019/05/M.svg',
+// 	12: 'https://www.esrb.org/wp-content/uploads/2019/05/AO.svg'
+// 	}
+// }
+
+const ESRB = [
+	{ 'IGDBRating': 6, 'src': 'https://www.esrb.org/wp-content/uploads/2021/12/RP-LM17-English.svg' },
+	{ 'IGDBRating': 7, 'src': 'https://www.esrb.org/wp-content/uploads/2019/05/RP.svg' },
+	{ 'IGDBRating': 8, 'src': 'https://www.esrb.org/wp-content/uploads/2019/05/E.svg' },
+	{ 'IGDBRating': 9, 'src': 'https://www.esrb.org/wp-content/uploads/2019/05/E10plus.svg' },
+	{ 'IGDBRating': 10, 'src':  'https://www.esrb.org/wp-content/uploads/2019/05/T.svg' },
+	{ 'IGDBRating': 11, 'src': 'https://www.esrb.org/wp-content/uploads/2019/05/M.svg' },
+	{ 'IGDBRating': 12, 'src': 'https://www.esrb.org/wp-content/uploads/2019/05/AO.svg' }
+]
+
+const PEGI = [
+	{ 'IGDBRating': 1, 'src': 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/2000px-PEGI_3.svg_.png?itok=v29cXS0R' },
+	{ 'IGDBRating': 2, 'src': 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/pegi7.png?itok=eI0Gk8v4' },
+	{ 'IGDBRating': 3, 'src': 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/PEGI_12.png?itok=YbTznZS9' },
+	{ 'IGDBRating': 4, 'src': 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/pegi16.png?itok=GyBvYeoU' },
+	{ 'IGDBRating': 5, 'src':  'https://pegi.info/sites/default/files/styles/medium/public/2017-03/pegi18.png?itok=2iF_eHO6' }
+]
+
+// const PEGI = {
+// 	1: 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/2000px-PEGI_3.svg_.png?itok=v29cXS0R',
+// 	2: 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/pegi7.png?itok=eI0Gk8v4',
+// 	3: 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/PEGI_12.png?itok=YbTznZS9',
+// 	4: 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/pegi16.png?itok=GyBvYeoU',
+// 	5: 'https://pegi.info/sites/default/files/styles/medium/public/2017-03/pegi18.png?itok=2iF_eHO6'
+// }
+
+export { ESRB, PEGI }
