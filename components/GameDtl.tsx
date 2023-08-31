2ca349afb0367aab5f57af73719abd790ac1d4f1
allthetok <al.tokjuman@gmail.com>
2023-08-30 20:50:59 -0400

    added description section

--- components/GameDtl.tsx
+++ components/GameDtl.tsx
@@ -259,7 +259,19 @@ const GameDtl = () => {
 			<div>
 				<h2>Official Description</h2>
 				<div className='shrink-headings toggle-long-text line-clamp'>
-
+					<p className='text-desc'>
+						{response.summary}
+					</p>
+					<p className='text-desc'>
+						In&nbsp;
+						<strong>
+							<em>{response.title}</em>
+						</strong>
+						,&nbsp;{response.story.charAt(0).toLowerCase() + response.story.slice(1)}
+					</p>
+					<p className='text-desc'>
+						Released by {response.involved_companies.map(company => company.name).join(', ')} on {formattedDateLong(response.releaseDate)}.
+					</p>
 				</div>
 			</div>
 		</div>
