b34847b93a2625ea860284bfefaa867aca1d74a9
allthetok <al.tokjuman@gmail.com>
2023-09-02 15:50:27 -0400

    created type for response from backend, added Overview component with pipeline to take in data from post request

--- components/GameDtl.tsx
+++ components/GameDtl.tsx
@@ -2,15 +2,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React, { useState, Suspense } from 'react'
-import { IconButton, Rating, Box, Button } from '@mui/material'
+import { IconButton } from '@mui/material'
 import AddBoxIcon from '@mui/icons-material/AddBox'
-import StarIcon from '@mui/icons-material/Star'
-// import { NavButton } from './NavButton'
 import { NavButtonList } from './NavButtonList'
+import { Overview } from './Overview'
 import { GameDetailObj, AgeRatings, Categories, Companies, Platforms, Videos, Languages } from '../../backendga/helpers/requests'
 import { response } from '../mockdata/response'
 import { ESRB, PEGI, ExternalCategories, WebsiteCategories } from '../assets/ratingsvglinks'
-import { ButtonSx, BoxSx, ButtonActiveSx, BoxActiveSx } from '../sxstyling/styles'
 import './GameDtl.css'
 
 const GameDtl = () => {
@@ -127,123 +125,7 @@ const GameDtl = () => {
 			<NavButtonList tabSelect={tabSelect} handleActiveChange={handleActiveChange}/>
 			{
 				tabSelect === 'overview' ?
-					<div id='infoBlock' className='game-info mb'>
-						<div className='info-box'>
-							<div>
-								<img className='img-box' alt='Cover Art of Game' src={response.cover}/>
-							</div>
-						</div>
-						<div className='info-releaservw'>
-							<dl className='releasedata'>
-								<dt>Released</dt>
-								<dd>
-									{formattedDateLong(response.releaseDate)}
-								</dd>
-								<dt>Platforms</dt>
-								<dd className='platforms'>
-									{getPlatformCompanies(response.platforms)}
-								</dd>
-								<dt>Developers & Publishers</dt>
-								<dd className='platforms'>
-									{getPlatformCompanies(response.involved_companies)}
-								</dd>
-								<dt>Age Ratings</dt>
-								<dd>
-									{getAgeRatings(response.age_ratings)}
-								</dd>
-								<dt>Localizations</dt>
-								<dd>
-									{response.game_localizations}
-								</dd>
-							</dl>
-						</div>
-						<div className='info-scores'>
-							<dl className='releasedata'>
-								<dt>IGDB Rating</dt>
-								<dd className='stars-rating'>
-									<Rating
-										name='rating'
-										value={ratingFloatToStar(response.rating)}
-										readOnly
-										precision={0.1}
-										emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize='inherit' />}
-										size='small'
-									/>
-									<p className='max-rating align'>
-								({response.ratingCount})
-									</p>
-								</dd>
-								<dt>User Reviews</dt>
-								<dd>
-									<span className='avg-rating'>
-										{parseFloat(response.rating.toString()).toFixed(2)}
-									</span>
-									<span className='max-rating'>
-								&nbsp;/ 100 based on
-									</span>
-									<span className='num-rating'>
-								&nbsp;{response.ratingCount}
-									</span>
-							&nbsp;
-									<span className='max-rating'>
-								reviews
-									</span>
-								</dd>
-								<dt>Hypes</dt>
-								<dd>
-							Hyped by {response.hypes} users pre-release!
-								</dd>
-								<dt>Follows</dt>
-								<dd>
-							Followed by {response.likes} users
-								</dd>
-								<dt>IGDB ID</dt>
-								<dd>
-									{response.id}
-								</dd>
-								<dt>IGDB Official Website</dt>
-								<dd>
-									<a href={response.url} target='_blank' rel='noreferrer'>{response.title}</a>
-								</dd>
-							</dl>
-						</div>
-						<div className='info-genres'>
-							<dl className='releasedata'>
-								<dt>Genres</dt>
-								<dd>
-									{getStringArr(response.genres)}
-								</dd>
-								<dt>Gameplay Perspective</dt>
-								<dd>
-									{getStringArr(response.player_perspectives)}
-								</dd>
-								<dt>Game Modes</dt>
-								<dd>
-									{getStringArr(response.game_modes)}
-								</dd>
-								<dt>Themes</dt>
-								<dd>
-									{getStringArr(response.themes)}
-								</dd>
-								<dt>Keywords</dt>
-								<dd>
-									{getStringArr(response.keywords)}
-								</dd>
-							</dl>
-						</div>
-						<div className='info-sites'>
-							<dl className='releasedata'>
-								<dt>Buy</dt>
-								<dd>
-									{getWebsites(response.external_games, 'External')}
-								</dd>
-								<dt>Official Sites</dt>
-								<dd>
-									{getWebsites(response.websites, 'Official')}
-								</dd>
-							</dl>
-						</div>
-					</div>
+					<Overview response={response} formattedDateLong={formattedDateLong} getPlatformCompanies={getPlatformCompanies} getAgeRatings={getAgeRatings} getStringArr={getStringArr} getWebsites={getWebsites} ratingFloatToStar={ratingFloatToStar} />
 					: <></>}
 			<div>
 				<h2>Official Description</h2>
--- helpers/types.ts
+++ helpers/types.ts
@@ -1,6 +1,39 @@
+import { AgeRatings, Categories, Companies, Languages, Platforms, Videos } from '../../backendga/helpers/requests'
+
 type ButtonField = {
 	stateSrc: string,
 	formatName: string
 }
 
-export type { ButtonField }
+type GameDetailObj = {
+	id: number | null,
+	age_ratings: AgeRatings,
+	artworks: string | string[],
+	cover: string | undefined,
+	external_games: Categories[],
+	game_modes: string[],
+	genres: string[],
+	hypes: number | null,
+	involved_companies: Companies[],
+	keywords: string[],
+	platforms: Platforms[],
+	player_perspectives: string[],
+	screenshots: string | string[],
+	similar_games: string | string[],
+	tags: string,
+	themes: string[],
+	videos: string | Videos[],
+	websites: Categories[],
+	language_supports: string | Languages[],
+	game_localizations: string,
+	rating: number,
+	ratingCount: number | null,
+	releaseDate:  string,
+	likes: number | null,
+	title: string,
+	story: string,
+	summary: string,
+	url: string
+}
+
+export type { ButtonField, GameDetailObj }
