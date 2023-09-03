ffa998594577bdb95e73e477cc4ea1c64fb33788
allthetok <al.tokjuman@gmail.com>
2023-09-03 15:42:39 -0400

    finished artworks component and responsive image styling

--- components/Artworks.tsx
+++ components/Artworks.tsx
@@ -2,6 +2,8 @@
 /* eslint-disable react/jsx-key */
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import { GameDetailObj } from '../helpers/types'
 import './Artworks.css'
 
@@ -14,15 +16,15 @@ const Artworks = ({ response, tabSelected }: ArtworksProps) => {
 	return (
 		<div>
 			{ tabSelected === 'artworks' ?
-				<Carousel>
+				<Carousel NextIcon={<ArrowForwardIcon/>} PrevIcon={<ArrowBackIcon/>} stopAutoPlayOnHover={true} interval={10000} animation={'fade'}>
 					{response.artworks.map((el: string) => (
-						<img src={el} alt='Artwork' />
+						<img className='image-carousel' src={el} alt='Artwork' />
 					))}
 				</Carousel>
 				:
-				<Carousel>
+				<Carousel NextIcon={<ArrowForwardIcon/>} PrevIcon={<ArrowBackIcon/>} stopAutoPlayOnHover={true} interval={10000} animation={'fade'}>
 					{response.screenshots.map((el: string) => (
-						<img src={el} alt='Artwork' />
+						<img className='image-carousel' src={el} alt='Artwork' />
 					))}
 				</Carousel>
 			}
