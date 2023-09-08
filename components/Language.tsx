aead6e6b78ba5f502e8a5e708caa2f992677e61f
allthetok <al.tokjuman@gmail.com>
2023-09-08 15:59:24 -0400

    filling table body with languages

--- components/Language.tsx
+++ components/Language.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react/jsx-key */
 import React from 'react'
 import { GameDetailObj } from '../helpers/types'
@@ -13,24 +14,64 @@ const TableCells = ({ response }: LanguageProps) => {
 	return (
 		<TableRow>
 			{supportTypes.map((item: any) => (
-				<TableCell align='center' sx={{ minWidth: 434.66 }}>
+				<TableCell align='center' sx={{ minWidth: 434.66, color: '#ddd' }}>
 					{item}
 				</TableCell>
 			))}
 		</TableRow>
 	)
 }
+
+const TableRows = ({ response }: LanguageProps) => {
+	const supportTypes = Array.from(new Set(response.language_supports.map((item: any) => item.language_support_type)))
+	const language = Array.from(new Set(response.language_supports.map((item: any) => (item.language))))
+	return (
+		<TableBody>
+			{language.map((row) => (
+				<div>
+					{filterEachRow(response.language_supports.filter((item: any) => item.language === row), language)}
+				</div>
+			))}
+		</TableBody>
+	)
+}
+
+const filterEachRow = (filteredArr: any, language: any) => {
+	console.log(filteredArr)
+	console.log(language)
+	return (
+		<TableRow
+			key='row'
+			sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+		>
+			{filteredArr.map((item: any) => {
+				(
+					<TableCell>
+						{item.language} {item.locale}
+					</TableCell>
+				)
+			})}
+		</TableRow>
+	)
+
+}
 const Language = ({ response }: LanguageProps) => {
 	return (
-		<div>
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 900, backgroundColor: '#1b1e22', color: '#ddd' }} aria-label='language table'>
-					<TableRow sx={{ textAlign: 'center' }}>
-						<TableCells response={response}/>
-					</TableRow>
-				</Table>
-			</TableContainer>
-		</div>
+		<>
+			<div>
+				<TableContainer component={Paper}>
+					<Table sx={{ minWidth: 900, backgroundColor: '#1b1e22' }} aria-label='language table'>
+						<TableHead>
+							<TableRow sx={{ textAlign: 'center' }}>
+								<TableCells response={response}/>
+							</TableRow>
+						</TableHead>
+						<TableRows response={response} />
+					</Table>
+				</TableContainer>
+			</div>
+		</>
+
 	)
 }
 
