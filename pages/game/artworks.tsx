bafcf204d2374ed9047343a1ee50e64efc3a963f
allthetok <al.tokjuman@gmail.com>
2023-09-13 16:07:57 -0400

    nested routing for game

---
+++ pages/game/artworks.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React, { Suspense } from 'react'
+import GameDtl from '../../components/GameDtl'
+import { Inter } from 'next/font/google'
+import '../../src/app/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+export default function Artworks() {
+	return (
+		<div>
+			Artworks
+		</div>
+	)
+}
+
+
+
+
