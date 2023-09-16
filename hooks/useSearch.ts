8cb3c55ec728cc52ea82cfc57013588b9f6efd76
allthetok <al.tokjuman@gmail.com>
2023-09-16 11:53:31 -0400

    custom hook useSearch for context

---
+++ hooks/useSearch.ts
@@ -0,0 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect, useCallback } from 'react'
+import { GameDetailObj } from '../helpers/types'
+import axios from 'axios'
+
+type UseSearchProps = {
+	gameSearch: string | null | undefined
+}
+
+const useSearch = ({ gameSearch	}: UseSearchProps) => {
+	const [dataFetch, setDataFetch] = useState<GameDetailObj>()
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
+
+	const searchConfig = {
+		method: 'post',
+		url: 'http://localhost:3001/api/gamedetails',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		data: {
+			'searchterm': gameSearch
+		}
+	}
+
+	const getGameDtl = useCallback(async () => {
+		await axios(searchConfig)
+			.then((response) => {
+				setDataFetch(response.data)
+				setLoading(false)
+			})
+			.catch((err) => {
+				setError(err)
+				console.error(err)
+
+			})
+	}, [gameSearch])
+
+	useEffect(() => {
+		getGameDtl()
+	}, [getGameDtl])
+
+	return [dataFetch, error, loading]
+}
+
+export { useSearch }
--- src/app/gamecontext.ts
+++ src/app/gamecontext.ts
@@ -1,31 +1,36 @@
 import { createContext } from 'react'
 import axios from 'axios'
 import { response } from '../../mockdata/response'
+import { useSearch } from '../../hooks/useSearch'
 
 // const GameContext = createContext(response)
-const getResponseObj = async (searchTerm: string) => {
-	const searchConfig = {
-		method: 'post',
-		url: 'http://localhost:3001/api/gamedetails',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		data : {
-			searchterm: 'witcher'
-		}
-	}
+// const getResponseObj = async (searchTerm: string) => {
+// 	const searchConfig = {
+// 		method: 'post',
+// 		url: 'http://localhost:3001/api/gamedetails',
+// 		headers: {
+// 			'Content-Type': 'application/json'
+// 		},
+// 		data : {
+// 			searchterm: 'witcher'
+// 		}
+// 	}
 
-	await axios(searchConfig)
-		.then((res) => {
-			console.log(res.data)
-		})
-		.catch((err) => {
-			console.log(err)
-		})
-	return response
-}
+// 	await axios(searchConfig)
+// 		.then((res) => {
+// 			console.log(res.data)
+// 		})
+// 		.catch((err) => {
+// 			console.log(err)
+// 		})
+// 	return response
+const GameContext = createContext(() => {
+	const [dataFetch, error, loading] = useSearch({ gameSearch: 'sekiro' })
+	return [dataFetch, error, loading]
 
-const GameContext = createContext(getResponseObj('sekiro'))
+})
+
+// const GameContext = createContext(getResponseObj('sekiro'))
 
 
 
