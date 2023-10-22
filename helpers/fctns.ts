a34c0265281c4cbc16edbcbfce244c1dfa1b282f
allthetok <al.tokjuman@gmail.com>
2023-10-22 18:23:42 -0400

    date formatting when game is yet to be released

--- helpers/fctns.ts
+++ helpers/fctns.ts
@@ -2,7 +2,7 @@ const ratingFloatToStar = (rating: number) : number => rating / 20
 
 const formattedDateLong = (inpDate: string) => new Date(inpDate).toLocaleDateString('en-us', { year: 'numeric', 'month': 'long', 'day': 'numeric' })
 
-const formattedYear = (inpDate: string) => new Date(inpDate).getFullYear()
+const formattedYear = (inpDate: string) => inpDate !== 'N/A' ? new Date(inpDate).getFullYear() : 'N/A'
 
 const createAxiosConfig = (method: string, endpoint: string, sortBy: string, sortDirection: string, platform: string, limit: string, genre: string) => {
 	return {
