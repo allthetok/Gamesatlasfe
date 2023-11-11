eb5ba5a87606c4ed5cfafc7b30cd3006a51b7c41
allthetok <al.tokjuman@gmail.com>
2023-11-11 12:35:12 -0500

    added api auth directory and simple files

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import type { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID!,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET!
+		}),
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				username: {
+					label: 'Username:',
+					type: 'text',
+					placeholder: 'Enter Username'
+				},
+				password: {
+					label: 'Password:',
+					type: 'password',
+					placeholder: 'Enter Password'
+				}
+			},
+			async authorize(credentials) {
+				//retrieve credentials from database configuration/providers/credentials
+				const user = { id: '42', name: 'Allen', password: 'tempuser' }
+
+				if (credentials?.username === user.name && credentials?.password === user.password) {
+					return user
+				}
+				else {
+					return null
+				}
+			}
+		})
+	]
+}
