fdd61abcd50695fe8507e4c5985ad07bdb67b24a
allthetok <al.tokjuman@gmail.com>
2023-11-16 22:38:47 -0500

    added all providers custom profile function

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -59,11 +59,94 @@ export const options: NextAuthOptions = {
 		),
 		SpotifyProvider({
 			clientId: process.env.SPOTIFY_CLIENT_ID as string,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
-		}),
+			clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+			profile: async function(profile) {
+				console.log(profile)
+				const oauthProviderConfig = {
+					method: 'post',
+					url: 'http://localhost:5000/api/loginOAuthUser',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					data: {
+						'email': profile.email,
+						'emailverified': false,
+						'username': profile.display_name,
+						'image': profile.images.length !== 0 ? profile.images[0] : '',
+						'externalId': profile.id
+					}
+				}
+				const internalUser = await axios(oauthProviderConfig)
+				.then((response: any) => {
+					if (response.status === 200) {
+						return {
+							id: response.data.id,
+							email: response.data.email,
+							username: response.data.username
+						}
+					}
+					else {
+						return null
+					}
+				})
+				.catch((err: any) => {
+					console.log(err)
+					return null
+				})
+				return {
+					email: profile.email,
+					name: profile.display_name,
+					image: profile.images.length !== 0 ? profile.images[0] : '',
+					id: internalUser?.id,
+					externalId: profile.id
+				}
+			}
+		}
+		),
 		DiscordProvider({
 			clientId: process.env.DISCORD_CLIENT_ID as string,
-			clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+			clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+			//broken at the moment, fix discord provider and compose up, delete volume now that email is not unique
+			profile: async function(profile) {
+				const oauthProviderConfig = {
+					method: 'post',
+					url: 'http://localhost:5000/api/loginOAuthUser',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					data: {
+						'email': profile.email,
+						'emailverified': profile.verified,
+						'username': profile.global_name,
+						'image': '',
+						'externalId': profile.id
+					}
+				}
+				const internalUser = await axios(oauthProviderConfig)
+				.then((response: any) => {
+					if (response.status === 200) {
+						return {
+							id: response.data.id,
+							email: response.data.email,
+							username: response.data.username
+						}
+					}
+					else {
+						return null
+					}
+				})
+				.catch((err: any) => {
+					console.log(err)
+					return null
+				})
+				return {
+					email: profile.email,
+					name: profile.global_name,
+					image: '',
+					id: internalUser?.id,
+					externalId: profile.id
+				}
+			}
 		}),
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID as string,
@@ -111,7 +194,48 @@ export const options: NextAuthOptions = {
 		}),
 		TwitchProvider({
 			clientId: process.env.TWITCH_CLIENT_ID as string,
-			clientSecret: process.env.TWITCH_CLIENT_SECRET as string
+			clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
+			profile: async function(profile) {
+				console.log(profile)
+				const oauthProviderConfig = {
+					method: 'post',
+					url: 'http://localhost:5000/api/loginOAuthUser',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					data: {
+						'email': profile.email,
+						'emailverified': false,
+						'username': profile.preferred_name,
+						'image': profile.picture,
+						'externalId': profile.sub
+					}
+				}
+				const internalUser = await axios(oauthProviderConfig)
+				.then((response: any) => {
+					if (response.status === 200) {
+						return {
+							id: response.data.id,
+							email: response.data.email,
+							username: response.data.username
+						}
+					}
+					else {
+						return null
+					}
+				})
+				.catch((err: any) => {
+					console.log(err)
+					return null
+				})
+				return {
+					email: profile.email,
+					name: profile.preferred_name,
+					image: profile.picture,
+					id: internalUser?.id,
+					externalId: profile.sub
+				}
+			}
 		}),
 		CredentialsProvider({
 			name: 'Credentials',
