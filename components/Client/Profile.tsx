8fb176c74f16350973c2b39c5c4dd86e13e200ea
allthetok <al.tokjuman@gmail.com>
2023-11-18 20:50:39 -0500

    started filling game preferences table

--- components/Client/Profile.tsx
+++ components/Client/Profile.tsx
@@ -6,7 +6,9 @@ import Image from 'next/image'
 import './Profile.css'
 import { Session } from 'next-auth'
 import { TableRows } from '@mui/icons-material'
-import { TableContainer, Paper, Table, TableHead, TableBody, TableRow, Box, Typography, TableCell } from '@mui/material'
+import { TableContainer, Paper, Table, TableHead, TableBody, TableRow, Box, Typography, TableCell, Button } from '@mui/material'
+import { genresButtonArray, platformButtonArray } from '../../helpers/button'
+import { BoxActiveSx, BoxNoBorderSx, ButtonActiveSx, ButtonSx } from '../../sxstyling/styles'
 
 type ProfileProps = {
 	// userData: Session | null
@@ -50,9 +52,8 @@ const Profile = ({ userData }: ProfileProps) => {
 							</div>
 							{/* </div> */}
 						</div>
-
 					</div>
-					<div className = 'account-details-wrapper'>
+					<div className='account-details-wrapper'>
 						<div className='account-table-wrapper'>
 							<Box sx={{ width: '500px' }}>
 								<Paper sx={{ width: '100%', mb: 2, color: '#ddd', backgroundColor: '#1b1e22' }}>
@@ -110,6 +111,75 @@ const Profile = ({ userData }: ProfileProps) => {
 													</TableCell>
 													<TableCell sx={{ color: '#ddd' }} component='td' align='right'>
 														{userData.data.user.externalId}
+													</TableCell>
+												</TableRow>
+											</TableBody>
+										</Table>
+									</TableContainer>
+								</Paper>
+							</Box>
+						</div>
+						<div className='game-preferences-table-wrapper'>
+							<Box sx={{ width: '950px' }}>
+								<Paper sx={{ width: '100%', mb: 2, color: '#ddd', backgroundColor: '#1b1e22' }}>
+									<Typography sx={{ flex: '1 1 100%', borderBottom: '1px solid white', fontWeight: '700' }} variant='h6' id='tableTitle' component='div'>
+										Game Preferences
+									</Typography>
+									<TableContainer component={Paper}>
+										<Table sx={{ minWidth: 850, color: '#ddd', backgroundColor: '#1b1e22' }} aria-label='Account Details table'>
+											<TableHead>
+											</TableHead>
+											<TableBody>
+												<TableRow sx={{ backgroundColor: '#1b1e22' }}>
+													<TableCell sx={{ color: '#ddd', minWidth: '50px' }} component='th' scope='row' padding='none'>
+														Your Platform:
+													</TableCell>
+													<TableCell sx={{ color: '#ddd', 'padding': '0' }} component='td' align='right'>
+														<ul className='adv-nav-tabs-table'>
+															{platformButtonArray.map((platform: string) => (
+																<li className='adv-nav-tabs-li' key={platform}>
+																	<Box>
+																		<Button sx={ButtonSx}>
+																			{platform}
+																		</Button>
+																	</Box>
+																</li>
+															))}
+														</ul>
+													</TableCell>
+												</TableRow>
+												<TableRow sx={{ backgroundColor: '#1b1e22' }}>
+													<TableCell sx={{ color: '#ddd' }} component='th' scope='row' padding='none'>
+														Favorite Genres:
+													</TableCell>
+													<TableCell sx={{ color: '#ddd', width: '900' }} component='td' align='right'>
+														<ul className='adv-nav-tabs-table'>
+															{genresButtonArray.map((genre: string) => (
+																<li className='adv-nav-tabs-li' key={genre}>
+																	<Box>
+																		<Button sx={ButtonSx}>
+																			{genre}
+																		</Button>
+																	</Box>
+																</li>
+															))}
+														</ul>
+													</TableCell>
+												</TableRow>
+												<TableRow sx={{ backgroundColor: '#1b1e22' }}>
+													<TableCell sx={{ color: '#ddd' }} component='th' scope='row' padding='none'>
+														Favorite Themes:
+													</TableCell>
+													<TableCell sx={{ color: '#ddd' }} component='td' align='right'>
+														{new Date(Number(userData.data.user.token.iat) * 1000).toLocaleString()}
+													</TableCell>
+												</TableRow>
+												<TableRow sx={{ backgroundColor: '#1b1e22' }}>
+													<TableCell sx={{ color: '#ddd' }} component='th' scope='row' padding='none'>
+														Favorite Game Type
+													</TableCell>
+													<TableCell sx={{ color: '#ddd' }} component='td' align='right'>
+														{userData.data.user.provider}
 													</TableCell>
 												</TableRow>
 											</TableBody>
