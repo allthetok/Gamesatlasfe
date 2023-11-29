9ab6424ae40f6a0c26702897912f55142b892c76
allthetok <al.tokjuman@gmail.com>
2023-11-28 22:22:52 -0500

    updates to recommendation page to show in list or table format

--- components/Client/Recommend.tsx
+++ components/Client/Recommend.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { useCallback, useEffect, useState } from 'react'
 import axios from 'axios'
@@ -5,7 +6,14 @@ import { createUserPrefSearchConfig } from '../../helpers/fctns'
 import { PreferencesRecList, ProfilePrefSearchConfig } from '../../helpers/fetypes'
 import { useSession } from 'next-auth/react'
 import ReactLoading from 'react-loading'
+import { IndGame } from './IndGame'
+import { IndGameTable } from './IndGameTable'
+import { Button, SvgIcon } from '@mui/material'
+import { ListTblToggleSx, IconSx } from '../../sxstyling/styles'
+import GridViewIcon from '@mui/icons-material/GridView'
+import TableRowsIcon from '@mui/icons-material/TableRows'
 import './Recommend.css'
+import './IndGameList.css'
 
 type RecommendProps = {
 	userData: any
@@ -14,8 +22,9 @@ type RecommendProps = {
 const Recommend = ({ userData }: RecommendProps) => {
 	const [loading, setLoading] = useState(true)
 	const [userPrefData, setUserPrefData] = useState({ platform: [], genres: [], themes: [], gamemodes: [] })
-	const [userPrefRecList, setUserPrefRecList] = useState<PreferencesRecList | null>(null)
+	const [userPrefRecList, setUserPrefRecList] = useState<PreferencesRecList[] | null>(null)
 	const [error, setError] = useState('')
+	const [viewToggle, setViewToggle] = useState('table')
 
 	const data = useSession()
 
@@ -71,9 +80,40 @@ const Recommend = ({ userData }: RecommendProps) => {
 
 	return (
 		<div>
-			Recommendations
-			{!loading ?
-				<></>
+			<div className='filter-recommend-wrap'>
+				<div className='button-wrap'>
+					<Button sx={ListTblToggleSx(viewToggle, 'listbtn')} onClick={() => setViewToggle('list')} variant={viewToggle === 'list' ? 'contained' : 'outlined'}>
+						<SvgIcon sx={IconSx} fontSize='large' htmlColor='#ddd'>
+							<GridViewIcon />
+						</SvgIcon>
+					</Button>
+					<Button sx={ListTblToggleSx(viewToggle, 'tblbtn')} onClick={() => setViewToggle('table')} variant={viewToggle === 'table' ? 'contained' : 'outlined'}>
+						<SvgIcon sx={IconSx} fontSize='large' htmlColor='#ddd'>
+							<TableRowsIcon />
+						</SvgIcon>
+					</Button>
+				</div>
+			</div>
+			{!loading && userPrefRecList ?
+				<>
+					{userPrefRecList.map((item: any) => (
+						<div className='explore-wrap'>
+							<div>
+								<h3 className='title-recommend'>
+									Based on Your Profile&apos;s {item.name.replace('User ', '')}
+								</h3>
+								{viewToggle === 'list' ?
+									<div className='grid-wrapper'>
+										{item.result.map((item: any) => (
+											<IndGame key={item.id} cover={item.cover!} platforms={item.platforms} rating={item.rating} age_ratings={item.age_ratings} releaseDate={item.releaseDate} likes={item.likes!} title={item.title} genres={item.genres} companies={item.involved_companies} />
+										))}
+									</div>
+									: <IndGameTable multiResp={item.result} />}
+							</div>
+						</div>
+
+					))}
+				</>
 				: <ReactLoading
 					type={'spinningBubbles'}
 					color={'#ddd'}
