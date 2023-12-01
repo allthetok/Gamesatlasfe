8d8912ab0ead7d3ed18246c90382a0dda3ebc7a8
allthetok <al.tokjuman@gmail.com>
2023-11-30 19:29:19 -0500

    finish footer styling

--- components/Client/Footer.tsx
+++ components/Client/Footer.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
-// import { Link } from '@mui/icons-material'
+import Link from 'next/link'
 import LinkIcon from '@mui/icons-material/Link'
 import { Typography } from '@mui/material'
-import Link from 'next/link'
+import './Footer.css'
 
 const Footer = () => {
-	return <Typography variant='body2' color='text.secondary' align='center' sx={{ bottom: '2.5%', left: '47.93%', position: 'fixed', fontSize: '1rem', color: '#daddeb', verticalAlign: 'middle' }}>
-		{'Copyright © '}
-
-		<Link target="_blank" rel="noopener noreferrer" href='https://github.com/allthetok' className=''>
-			<LinkIcon sx={{ verticalAlign: 'bottom', '&hover': { cursor: 'pointer' } }}>
-			</LinkIcon>
-		</Link>
-		{' '}
-		{/* <Link color='inherit' href='https://github.com/allthetok' sx={{ verticalAlign: 'bottom', '&hover': { cursor: 'pointer' } }}>
-            Allen Tokjuman
-		</Link>{' '} */}
-		Allen Tok{'. '}
-		{new Date().getFullYear()}
-	</Typography>
+	return (
+		<div>
+			<div className='footer-background'></div>
+			<Typography variant='body2' color='text.secondary' align='center' sx={{ width: '100%', bottom: '1.25%', position: 'fixed', fontSize: '1rem', color: '#daddeb', verticalAlign: 'middle' }}>
+				{'Copyright © '}
+				<Link target='_blank' rel='noopener noreferrer' href='https://github.com/allthetok' className='git-link'>
+					<LinkIcon sx={{ verticalAlign: 'bottom', '&hover': { cursor: 'pointer' } }} />
+				</Link>
+				{' '}Allen Tokjuman{' - '}
+				{new Date().getFullYear()}
+			</Typography>
+		</div>
+	)
 }
 
 export { Footer }
