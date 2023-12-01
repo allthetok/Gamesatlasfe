96d39f7133fc9d942caeaa511a77a75a653994d6
allthetok <al.tokjuman@gmail.com>
2023-11-30 22:01:41 -0500

    more styling and limit button working

--- components/Client/Recommend.tsx
+++ components/Client/Recommend.tsx
@@ -14,10 +14,10 @@ import { ThemeProvider } from '@mui/material/styles'
 import { ListTblToggleSx, IconSx } from '../../sxstyling/styles'
 import GridViewIcon from '@mui/icons-material/GridView'
 import TableRowsIcon from '@mui/icons-material/TableRows'
-import './Recommend.css'
-import './IndGameList.css'
 import { Explore } from '../../helpers/fetypes'
 import { theme } from '../../sxstyling/theme'
+import './Recommend.css'
+import './IndGameList.css'
 
 
 type RecommendProps = {
@@ -25,16 +25,16 @@ type RecommendProps = {
 }
 
 const Recommend = ({ userData }: RecommendProps) => {
+	const [error, setError] = useState('')
 	const [loading, setLoading] = useState(true)
 	const [userPrefList, setUserPrefList] = useState<PreferencesRecList[]>([])
 	const [userSimilarRecList, setUserSimilarRecList] = useState<Explore[]>([])
-	const [error, setError] = useState('')
 	const [viewToggle, setViewToggle] = useState('list')
 	const [limit, setLimit] = useState('10')
 
 	const data = useSession()
 
-	const numOptions = ['5', '10', '25', '50']
+	const numOptions = ['7', '10', '25', '50']
 
 	const onLimitChange = (e: SyntheticEvent<Element, Event>, value: string | null): void => {
 		e.preventDefault()
@@ -65,36 +65,27 @@ const Recommend = ({ userData }: RecommendProps) => {
 			.then((response: AxiosResponse) => {
 				const prefRecList = response.data.map((item: any) => item.recommendobjarr)
 				const prefRecListJoined = Array.prototype.concat(...prefRecList)
-				setUserSimilarRecList(prefRecListJoined)
+				setUserSimilarRecList(prefRecListJoined.slice(0, Number(limit)))
 			})
 			.catch((err: AxiosError) => {
 				setError('Unable to retrieve recommendations based on your Profile Game Preferences')
 				console.error(err)
 			})
 		if (prefArrayData.platform.length !== 0 || prefArrayData.genres.length !== 0 || prefArrayData.themes.length !== 0 || prefArrayData.gamemodes.length !== 0) {
-			const userPrefSearchConfig: ProfilePrefSearchConfig = createUserPrefSearchConfig('post', 'recommendPrefs', prefArrayData.platform, prefArrayData.genres, prefArrayData.themes, prefArrayData.gamemodes, 'age_ratings, follows, involved_companies, game_modes, category, total_rating', 25, 'IGDB Rating', 'desc')
+			const userPrefSearchConfig: ProfilePrefSearchConfig = createUserPrefSearchConfig('post', 'recommendPrefs', prefArrayData.platform, prefArrayData.genres, prefArrayData.themes, prefArrayData.gamemodes, 'age_ratings, follows, involved_companies, game_modes, category, total_rating', Number(limit), 'IGDB Rating', 'desc')
 			await axios(userPrefSearchConfig)
 				.then((response) => {
-					console.log(response.data)
-					// const resultPrefList: PreferencesRecList = {
-					// 	platform: response.data.filter((multiq: any) => multiq.name === 'Platforms').length !== 0 ? response.data.filter((multiq: any) => multiq.name === 'Platforms')[0].result : [],
-					// 	genres: response.data.filter((multiq: any) => multiq.name === 'Genres').length !== 0 ? response.data.filter((multiq: any) => multiq.name === 'Genres')[0].result : [],
-					// 	themes: response.data.filter((multiq: any) => multiq.name === 'Themes').length !== 0 ? response.data.filter((multiq: any) => multiq.name === 'Themes')[0].result : [],
-					// 	gamemodes: response.data.filter((multiq: any) => multiq.name === 'Game Modes').length !== 0 ? response.data.filter((multiq: any) => multiq.name === 'Game Modes')[0].result : [],
-					// }
+					// console.log(response.data)
 					setUserPrefList(response.data)
-
-					// setUserPrefList({
-					// 	platform: response.data.filter((multiq: any) => multiq.name === 'Platforms').length !== 0 ? response.data.filter((multiq: any) => multiq.name === 'Platforms')[0].result : [],
-
-					// })
 					setLoading(false)
 				})
 				.catch((err) => {
 					setError('Unable to retrieve recommendations based on your Profile Game Preferences')
 					console.error(err)
-
 				})
+		}
+		else {
+			setLoading(false)
 		}
 	}
 
@@ -102,11 +93,23 @@ const Recommend = ({ userData }: RecommendProps) => {
 		if (data.status === 'authenticated') {
 			getUserPrefProfile(userData.data.user.id, userData.data.user.profileid)
 		}
-	}, [data])
+	}, [data, limit])
 
 	return (
 		<div>
 			<div className='filter-recommend-wrap'>
+				<div className='drop-wrap'>
+					<ThemeProvider theme={theme}>
+						<Autocomplete
+							className='auto-comp'
+							disablePortal
+							id='combo-box'
+							options={numOptions}
+							value={limit}
+							onChange={onLimitChange}
+							sx={{ width: 150, bgcolor: '#ddd', backgroundColor: '#121212', borderRadius: '10px', marginRight: '0.75rem' }} renderInput={(params) => <TextField {...params} label="Limit" />} />
+					</ThemeProvider>
+				</div>
 				<div className='button-wrap'>
 					<Button sx={ListTblToggleSx(viewToggle, 'listbtn')} onClick={() => setViewToggle('list')} variant={viewToggle === 'list' ? 'contained' : 'outlined'}>
 						<SvgIcon sx={IconSx} fontSize='large' htmlColor='#ddd'>
@@ -120,35 +123,27 @@ const Recommend = ({ userData }: RecommendProps) => {
 					</Button>
 				</div>
 			</div>
-			<div className='drop-wrap'>
-				<ThemeProvider theme={theme}>
-					<Autocomplete
-						className='auto-comp'
-						disablePortal
-						id='combo-box'
-						options={numOptions}
-						value={limit}
-						onChange={onLimitChange}
-						sx={{ width: 150, bgcolor: '#ddd', backgroundColor: '#121212', borderRadius: '10px', marginRight: '0.75rem' }} renderInput={(params) => <TextField {...params} label="Limit" />} />
-				</ThemeProvider>
-			</div>
 			<div className='explore-wrap'>
 				<h3 className='title-recommend'>
 						Based on Games You Like
 				</h3>
-				{userSimilarRecList ?
+				{!loading && userSimilarRecList.length !== 0 ?
 					<>
 						{viewToggle === 'list' ?
 							<div className='grid-wrapper'>
 								{userSimilarRecList.map((item: any) => (
-									<IndGame key={item.id} cover={item.cover!} platforms={item.platforms} rating={item.rating} age_ratings={item.age_ratings} releaseDate={item.releaseDate} likes={item.likes!} title={item.title} genres={item.genres} companies={item.involved_companies} />
+									<IndGame key={item.index} cover={item.cover!} platforms={item.platforms} rating={item.rating} age_ratings={item.age_ratings} releaseDate={item.releaseDate} likes={item.likes!} title={item.title} genres={item.genres} companies={item.involved_companies} />
 								))}
 							</div>
 							: <IndGameTable multiResp={userSimilarRecList} />}
 					</>
-					: <></>
+					: <ReactLoading
+						type={'spinningBubbles'}
+						color={'#ddd'}
+						height={150}
+						width={150}/>}
 			</div>
-			{!loading && userPrefList ?
+			{!loading && userPrefList.length !== 0 ?
 				<>
 					{userPrefList.map((item: any) => (
 						<div className='explore-wrap'>
@@ -159,7 +154,7 @@ const Recommend = ({ userData }: RecommendProps) => {
 								{viewToggle === 'list' ?
 									<div className='grid-wrapper'>
 										{item.result.map((item: any) => (
-											<IndGame key={item.id} cover={item.cover!} platforms={item.platforms} rating={item.rating} age_ratings={item.age_ratings} releaseDate={item.releaseDate} likes={item.likes!} title={item.title} genres={item.genres} companies={item.involved_companies} />
+											<IndGame key={item.index} cover={item.cover!} platforms={item.platforms} rating={item.rating} age_ratings={item.age_ratings} releaseDate={item.releaseDate} likes={item.likes!} title={item.title} genres={item.genres} companies={item.involved_companies} />
 										))}
 									</div>
 									: <IndGameTable multiResp={item.result} />}
