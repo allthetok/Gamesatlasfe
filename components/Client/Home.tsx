24ad10e532f0b57a9f17f8a690ad5fb14488879a
allthetok <al.tokjuman@gmail.com>
2023-12-04 18:49:14 -0500

    home example of carousel

--- components/Client/Home.tsx
+++ components/Client/Home.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { homeData } from '../../mockdata/homedata'
+import Carousel from 'react-material-ui-carousel'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { IndGame } from './IndGame'
+import './Artworks.css'
+import './GameDtl.css'
+import './IndGameList.css'
 
-const Home = () => {
+const HomeCarousel = () => {
+	const homeDataSliced = homeData.slice(0, 7)
+
+
 	return (
-		<div>
-			HomePage Hold
+		<div className='home-wrapper'>
+			<Carousel NextIcon={<ArrowForwardIcon/>} PrevIcon={<ArrowBackIcon/>} stopAutoPlayOnHover={true} interval={10000} animation={'fade'}>
+				<div className='grid-wrapper'>
+					{homeDataSliced.map((el: any) => (
+						<IndGame key={el.index} id={el.id} cover={el.cover} platforms={el.platforms} rating={el.rating} age_ratings={el.age_ratings} releaseDate={el.releaseDate} likes={el.likes} title={el.title} genres={el.genres} companies={el.involved_companies} liked={false}  />
+					))}
+				</div>
+			</Carousel>
 		</div>
 	)
 }
 
-export { Home }
+export { HomeCarousel }
