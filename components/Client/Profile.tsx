69cf02dd5c99b2ba09d21f23ea1de59d69174fa5
allthetok <al.tokjuman@gmail.com>
2023-12-05 20:42:07 -0500

    media queries for profile page

--- components/Client/Profile.tsx
+++ components/Client/Profile.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react'
