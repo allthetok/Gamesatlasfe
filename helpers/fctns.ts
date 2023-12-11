989e0afe039f5d0eef65a562e23c57fe1618b6d3
allthetok <al.tokjuman@gmail.com>
2023-12-10 22:33:24 -0500

    update to use digitalocean VM server

--- helpers/fctns.ts
+++ helpers/fctns.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 require('dotenv').config()
+import { prodIPV4 } from '../prod-env/env-var'
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import { LocalStorageObj, NestedSearchConfig, ProfilePrefSearchConfig, SimpleNullableSearchConfig, SimpleSearchConfig } from './fetypes'
 
@@ -14,7 +15,7 @@ const formattedYear = (inpDate: string | Date) => inpDate !== 'N/A' ? new Date(i
 const createExploreAxiosConfig = (method: string, endpoint: string, sortBy: string, sortDirection: string, platform: string, limit: string, genre: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/game/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/game/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -33,7 +34,7 @@ const createExploreAxiosConfig = (method: string, endpoint: string, sortBy: stri
 const createAdvancedAxiosConfig = (method: string, endpoint: string, sortBy: string, sortDirection: string, limit: string, platforms: string[], genres: string[], themes: string[], gameModes: string[], categories: string[], rating: number[], releaseDate: number[], companies: string[]) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/game/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/game/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -59,7 +60,7 @@ const createAdvancedAxiosConfig = (method: string, endpoint: string, sortBy: str
 const createDeprecatedGameDtlConfig = (method: string, endpoint: string, searchTerm: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/deprecated/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/deprecated/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -72,7 +73,7 @@ const createDeprecatedGameDtlConfig = (method: string, endpoint: string, searchT
 const createDeprecatedNestedConfig = (method: string, endpoint: string, gameid: number) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/deprecated/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/deprecated/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -93,7 +94,7 @@ const createGameDtlConfig = (method: string, endpoint: string, searchTerm: strin
 	}
 	return {
 		method: method,
-		url: `http://localhost:5000/api/game/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/game/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -114,7 +115,7 @@ const createInnerSearchConfig = (method: string, endpoint: string, searchTerm: s
 	}
 	return {
 		method: method,
-		url: `http://localhost:5000/api/game/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/game/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -136,7 +137,7 @@ const createAuxiliaryConfig = (method: string, endpoint: string, gameID: string 
 	}
 	return {
 		method: method,
-		url: `http://localhost:5000/api/game/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/game/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -149,7 +150,7 @@ const createAuxiliaryConfig = (method: string, endpoint: string, gameID: string 
 const createUserPrefSearchConfig = (method: string, endpoint: string, platforms: string[], genres: string[], themes: string[], gameModes: string[], nullable: string, limit: number, sortBy: string, sortDirection: string): ProfilePrefSearchConfig => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -169,7 +170,7 @@ const createUserPrefSearchConfig = (method: string, endpoint: string, platforms:
 const createUserRecommendConfig = (method: string, endpoint: string, userid: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -182,7 +183,7 @@ const createUserRecommendConfig = (method: string, endpoint: string, userid: str
 const createUserDetailsConfig = (method: string, endpoint: string, userid: string, profileid: string, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -197,7 +198,7 @@ const createUserDetailsConfig = (method: string, endpoint: string, userid: strin
 const createUserProfileConfig = (method: string, endpoint: string, userid: string, profileid: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -225,7 +226,7 @@ const createUserLikeConfig = (method: string, endpoint: string, userid: string, 
 
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -241,7 +242,7 @@ const createUserLikeConfig = (method: string, endpoint: string, userid: string, 
 const createUserDeleteConfig = (method: string, endpoint: string, userid: string, gameid: string | number) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -255,7 +256,7 @@ const createUserDeleteConfig = (method: string, endpoint: string, userid: string
 const createUserPatchConfig = (method: string, endpoint: string, userid: string, profileid: string, provider: string, specField: string, email: string, username: string, password: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -274,7 +275,7 @@ const createUserPatchConfig = (method: string, endpoint: string, userid: string,
 const createUserGenericConfig = (method: string, endpoint: string, userid: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -287,7 +288,7 @@ const createUserGenericConfig = (method: string, endpoint: string, userid: strin
 const createUserEmailConfig = (method: string, endpoint: string, email: string | undefined, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -301,7 +302,7 @@ const createUserEmailConfig = (method: string, endpoint: string, email: string |
 const createUserNameConfig = (method: string, endpoint: string, name: string, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -315,7 +316,7 @@ const createUserNameConfig = (method: string, endpoint: string, name: string, pr
 const createLoginConfig = (method: string, endpoint: string, email: string | undefined, password: string | undefined, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -330,7 +331,7 @@ const createLoginConfig = (method: string, endpoint: string, email: string | und
 const createSignUpConfig = (method: string, endpoint: string, email: string, name: string, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -345,7 +346,7 @@ const createSignUpConfig = (method: string, endpoint: string, email: string, nam
 const createFullSignUpConfig = (method: string, endpoint: string, email: string | undefined, name: string | undefined, password: string | undefined, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -361,7 +362,7 @@ const createFullSignUpConfig = (method: string, endpoint: string, email: string 
 const createGamePrefPatchConfig = (method: string, endpoint: string, userid: string, profileid: string, platforms: string[], genres: string[], themes: string[], gameModes: string[]) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
@@ -379,7 +380,7 @@ const createGamePrefPatchConfig = (method: string, endpoint: string, userid: str
 const createOAuthConfig = (method: string, endpoint: string, email: string, emailverified: boolean | null | undefined, username: string, image: string, externalId: string, provider: string) => {
 	return {
 		method: method,
-		url: `http://localhost:5000/api/user/${endpoint}`,
+		url: `http://${prodIPV4.url}:5000/api/user/${endpoint}`,
 		headers: {
 			'Content-Type': 'application/json'
 		},
